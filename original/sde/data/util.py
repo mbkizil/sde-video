0d7d38f0e7e7801c3ac3b5b5a5121db5bce65016
mbkizil <63160324+mbkizil@users.noreply.github.com>
2024-07-16 13:32:33 +0300

    Update util.py

--- original/sde/data/util.py
+++ original/sde/data/util.py
@@ -1,11 +1,18 @@
 from .moving_mnist import MovingMNIST
 from .bounce import Bounce
+import random
+from functools import partial
+from itertools import repeat
+from typing import Callable
+from timm.data.distributed_sampler import OrderedDistributedSampler, RepeatAugSampler
 
-
+import torch.utils.data
+import numpy as np
+'''
 def get(dataset):
     if dataset == 'mmnist':
-        data_train = MovingMNIST(train=True, data_root='data', seq_len=25, num_digits=2, deterministic=False)
-        data_val = MovingMNIST(train=False, data_root='data', seq_len=25, num_digits=2, deterministic=False)
+        data_train = MovingMNIST(train=True, data_root='/kuacc/users/mkizil19/hpc_run/sdevideo/sdevideo/VideoAsAStochasticProcess-main/sde/data', seq_len=20, num_digits=2, deterministic=False)
+        data_val = MovingMNIST(train=False, data_root='/kuacc/users/mkizil19/hpc_run/sdevideo/sdevideo/VideoAsAStochasticProcess-main/sde/data', seq_len=20, num_digits=2, deterministic=False)
         image_size = 64
         num_channels = 1
         dt = data_train.step_length
@@ -24,3 +31,300 @@ def get(dataset):
         'dt': dt,
     }
     return data_train, data_val, dataset_kwargs
+'''
+def worker_init(worker_id, worker_seeding='all'):
+    worker_info = torch.utils.data.get_worker_info()
+    assert worker_info.id == worker_id
+    if isinstance(worker_seeding, Callable):
+        seed = worker_seeding(worker_info)
+        random.seed(seed)
+        torch.manual_seed(seed)
+        np.random.seed(seed % (2 ** 32 - 1))
+    else:
+        assert worker_seeding in ('all', 'part')
+        # random / torch seed already called in dataloader iter class w/ worker_info.seed
+        # to reproduce some old results (same seed + hparam combo), partial seeding
+        # is required (skip numpy re-seed)
+        if worker_seeding == 'all':
+            np.random.seed(worker_info.seed % (2 ** 32 - 1))
+
+
+def fast_collate_for_prediction(batch):
+    """ A fast collation function optimized for float32 images (np array or torch)
+        and float32 targets (video prediction labels) in video prediction tasks"""
+    assert isinstance(batch[0], tuple)
+    batch_size = len(batch)
+    if isinstance(batch[0][0], tuple):
+        # This branch 'deinterleaves' and flattens tuples of input tensors into
+        # one tensor ordered by position such that all tuple of position n will end up
+        # in a torch.split(tensor, batch_size) in nth position
+        inner_tuple_size = len(batch[0][0])
+        flattened_batch_size = batch_size * inner_tuple_size
+        targets = torch.zeros(flattened_batch_size, dtype=np.float32)
+        tensor = torch.zeros((flattened_batch_size, *batch[0][0][0].shape), dtype=np.float32)
+        for i in range(batch_size):
+            # all input tensor tuples must be same length
+            assert len(batch[i][0]) == inner_tuple_size
+            for j in range(inner_tuple_size):
+                targets[i + j * batch_size] = batch[i][1]
+                tensor[i + j * batch_size] += torch.from_numpy(batch[i][0][j])
+        return tensor, targets
+    elif isinstance(batch[0][0], np.ndarray):
+        targets = torch.tensor([b[1] for b in batch], dtype=np.float32)
+        assert len(targets) == batch_size
+        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=np.float32)
+        for i in range(batch_size):
+            tensor[i] += torch.from_numpy(batch[i][0])
+        return tensor, targets
+    elif isinstance(batch[0][0], torch.Tensor):
+        targets = torch.zeros((batch_size, *batch[1][0].shape), dtype=np.float32)
+        assert len(targets) == batch_size
+        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=np.float32)
+        for i in range(batch_size):
+            tensor[i].copy_(batch[i][0])
+        return tensor, targets
+    else:
+        assert False
+
+
+def expand_to_chs(x, n):
+    if not isinstance(x, (tuple, list)):
+        x = tuple(repeat(x, n))
+    elif len(x) == 1:
+        x = x * n
+    else:
+        assert len(x) == n, 'normalization stats must match image channels'
+    return x
+
+
+class PrefetchLoader:
+
+    def __init__(self,
+                 loader,
+                 mean=None,
+                 std=None,
+                 channels=3,
+                 fp16=False):
+
+        self.fp16 = fp16
+        self.loader = loader
+        if mean is not None and std is not None:
+            mean = expand_to_chs(mean, channels)
+            std = expand_to_chs(std, channels)
+            normalization_shape = (1, channels, 1, 1)
+
+            self.mean = torch.tensor([x * 255 for x in mean]).cuda().view(normalization_shape)
+            self.std = torch.tensor([x * 255 for x in std]).cuda().view(normalization_shape)        
+            if fp16:
+                self.mean = self.mean.half()
+                self.std = self.std.half()
+        else:
+            self.mean, self.std = None, None
+
+    def __iter__(self):
+        stream = torch.cuda.Stream()
+        first = True
+
+        for next_input, next_target in self.loader:
+            with torch.cuda.stream(stream):
+                next_input = next_input.cuda(non_blocking=True)
+                next_target = next_target.cuda(non_blocking=True)
+                if self.fp16:
+                    if self.mean is not None:
+                        next_input = next_input.half().sub_(self.mean).div_(self.std)
+                        next_target = next_target.half().sub_(self.mean).div_(self.std)
+                    else:
+                        next_input = next_input.half()
+                        next_target = next_target.half()
+                else:
+                    if self.mean is not None:
+                        next_input = next_input.float().sub_(self.mean).div_(self.std)
+                        next_target = next_target.float().sub_(self.mean).div_(self.std)
+                    else:
+                        next_input = next_input.float()
+                        next_target = next_target.float()
+
+            if not first:
+                yield input, target
+            else:
+                first = False
+
+            torch.cuda.current_stream().wait_stream(stream)
+            input = next_input
+            target = next_target
+
+        yield input, target
+
+    def __len__(self):
+        return len(self.loader)
+
+    @property
+    def sampler(self):
+        return self.loader.sampler
+
+    @property
+    def dataset(self):
+        return self.loader.dataset
+
+
+def create_loader(dataset,
+                  batch_size,
+                  shuffle=True,
+                  is_training=False,
+                  mean=None,
+                  std=None,
+                  num_workers=1,
+                  num_aug_repeats=0,
+                  input_channels=1,
+                  use_prefetcher=False,
+                  distributed=False,
+                  pin_memory=False,
+                  drop_last=True,
+                  fp16=False,
+                  collate_fn=None,
+                  persistent_workers=True,
+                  worker_seeding='all'):
+    sampler = None
+    if distributed and not isinstance(dataset, torch.utils.data.IterableDataset):
+        if is_training:
+            if num_aug_repeats:
+                sampler = RepeatAugSampler(dataset, num_repeats=num_aug_repeats)
+            else:
+                sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        else:
+            # This will add extra duplicate entries to result in equal num
+            # of samples per-process, will slightly alter validation results
+            sampler = OrderedDistributedSampler(dataset)
+    else:
+        assert num_aug_repeats==0, "RepeatAugment is not supported in non-distributed or IterableDataset"
+
+    if collate_fn is None:
+        collate_fn = torch.utils.data.dataloader.default_collate
+    loader_class = torch.utils.data.DataLoader
+
+    loader_args = dict(
+        batch_size=batch_size,
+        shuffle=shuffle and (not isinstance(dataset, torch.utils.data.IterableDataset)) and sampler is None and is_training,
+        num_workers=num_workers,
+        sampler=sampler,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+        drop_last=drop_last,
+        worker_init_fn=partial(worker_init, worker_seeding=worker_seeding),
+        persistent_workers=persistent_workers
+    )
+    try:
+        loader = loader_class(dataset, **loader_args)
+    except TypeError:
+        loader_args.pop('persistent_workers')  # only in Pytorch 1.7+
+        loader = loader_class(dataset, **loader_args)
+
+    if use_prefetcher:
+        loader = PrefetchLoader(
+            loader,
+            mean=mean,
+            std=std,
+            channels=input_channels,
+            fp16=fp16,
+        )
+
+    return loader
+
+
+def reshape_patch(img_tensor, patch_size):
+    assert 4 == img_tensor.ndim
+    seq_length = np.shape(img_tensor)[0]
+    img_height = np.shape(img_tensor)[1]
+    img_width = np.shape(img_tensor)[2]
+    num_channels = np.shape(img_tensor)[3]
+    a = np.reshape(img_tensor, [seq_length,
+                                img_height // patch_size, patch_size,
+                                img_width // patch_size, patch_size,
+                                num_channels])
+    b = np.transpose(a, [0, 1, 3, 2, 4, 5])
+    patch_tensor = np.reshape(b, [seq_length,
+                                  img_height // patch_size,
+                                  img_width // patch_size,
+                                  patch_size * patch_size * num_channels])
+    return patch_tensor
+
+
+def reshape_patch_back(patch_tensor, patch_size):
+    # B L H W C
+    assert 5 == patch_tensor.ndim
+    batch_size = np.shape(patch_tensor)[0]
+    seq_length = np.shape(patch_tensor)[1]
+    patch_height = np.shape(patch_tensor)[2]
+    patch_width = np.shape(patch_tensor)[3]
+    channels = np.shape(patch_tensor)[4]
+    img_channels = channels // (patch_size * patch_size)
+    a = np.reshape(patch_tensor, [batch_size, seq_length,
+                                  patch_height, patch_width,
+                                  patch_size, patch_size,
+                                  img_channels])
+    b = np.transpose(a, [0, 1, 2, 4, 3, 5, 6])
+    img_tensor = np.reshape(b, [batch_size, seq_length,
+                                patch_height * patch_size,
+                                patch_width * patch_size,
+                                img_channels])
+    return img_tensor
+
+
+def reshape_patch_back_tensor(patch_tensor, patch_size):
+    # B L H W C
+    assert 5 == patch_tensor.ndim
+    patch_narray = patch_tensor.detach().cpu().numpy()
+    batch_size = np.shape(patch_narray)[0]
+    seq_length = np.shape(patch_narray)[1]
+    patch_height = np.shape(patch_narray)[2]
+    patch_width = np.shape(patch_narray)[3]
+    channels = np.shape(patch_narray)[4]
+    img_channels = channels // (patch_size * patch_size)
+    a = torch.reshape(patch_tensor, [batch_size, seq_length,
+                                     patch_height, patch_width,
+                                     patch_size, patch_size,
+                                     img_channels])
+    b = a.permute([0, 1, 2, 4, 3, 5, 6])
+    img_tensor = torch.reshape(b, [batch_size, seq_length,
+                                   patch_height * patch_size,
+                                   patch_width * patch_size,
+                                   img_channels])
+    return img_tensor.permute(0, 1, 4, 2, 3)
+
+
+def get(batch_size, val_batch_size=32, data_root='/kuacc/users/mkizil19/hpc_run/sdevideo/sdevideo/original/sde/data', num_workers=2, data_name='mnist',
+              pre_seq_length=10, aft_seq_length=10, in_shape=[10, 1, 64, 64],
+              distributed=False, use_augment=False, use_prefetcher=False, drop_last=True):
+
+    image_size = in_shape[-1] if in_shape is not None else 64
+    train_set = MovingMNIST(root=data_root, is_train=True, data_name=data_name,
+                            n_frames_input=pre_seq_length,
+                            n_frames_output=aft_seq_length, num_objects=[2],
+                            image_size=image_size, use_augment=use_augment)
+    test_set = MovingMNIST(root=data_root, is_train=False, data_name=data_name,
+                           n_frames_input=pre_seq_length,
+                           n_frames_output=aft_seq_length, num_objects=[2],
+                           image_size=image_size, use_augment=False)
+    
+    #print("train_set: ", train_set[0])
+    #train_set = train_set.numpy()
+    #test_set = torch.Tensor.numpy(test_set)
+    dataloader_train = create_loader(train_set,
+                                    batch_size=batch_size,
+                                    shuffle=True, is_training=True,
+                                    pin_memory=True, drop_last=True,
+                                    num_workers=num_workers, persistent_workers=True,
+                                    distributed=distributed, use_prefetcher=use_prefetcher)
+    dataloader_vali = create_loader(test_set,
+                                    batch_size=val_batch_size,
+                                    shuffle=False, is_training=False,
+                                    pin_memory=True, drop_last=drop_last,
+                                    num_workers=num_workers,
+                                    distributed=distributed, use_prefetcher=use_prefetcher)
+    dataset_kwargs = {
+        'image_size': image_size,
+        'num_channels': in_shape[1],
+        'dt': 0.1,
+    }
+
+    return dataloader_train, dataloader_vali, dataset_kwargs
